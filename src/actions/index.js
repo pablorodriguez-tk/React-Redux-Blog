a5c534b656139f11d6a601ac1f052d472d91b5ec
Pablo Rodriguez <pablorodriguez88@gmail.com>
2021-03-17 14:33:10 -0300

    Segundo commit

---
+++ src/actions/index.js
@@ -0,0 +1,8 @@
+import jsonplaceholder from "../apis/jsonPlaceholder";
+
+//Action creactor
+export const fetchPosts = () => async (dispatch) => {
+  const response = await jsonplaceholder.get("/posts");
+
+  dispatch({ type: "FETCH_POSTS", payload: response });
+};
